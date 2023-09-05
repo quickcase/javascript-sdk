b2fae3bc57ac33a1106a9e3d82b6db16ea05ea1b
Valentin Laurin <valentin.laurin@quickcase.app>
2023-09-05 17:06:37 +0100

    [Metadata] Register constants for metadata items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './polyfills.js';
+export * as Metadata from './metadata/index.js';
---
+++ src/metadata/constants.js
@@ -0,0 +1,8 @@
+export const WORKSPACE = '[workspace]';
+export const TYPE = '[type]';
+export const REFERENCE = '[reference]';
+export const TITLE = '[title]';
+export const STATE = '[state]';
+export const CLASSIFICATION = '[classification]';
+export const CREATED_AT = '[createdAt]';
+export const LAST_MODIFIED_AT = '[lastModifiedAt]';
