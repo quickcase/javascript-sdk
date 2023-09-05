8fb437043e9ead838aa42a89104ab30cbf09fd4f
Valentin Laurin <valentin.laurin@quickcase.app>
2023-09-05 12:54:18 +0100

    Set up ESM support
    Use ES modules for code and tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-const path = require('path');
+import {dirname, resolve} from 'path';
+import {fileURLToPath} from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
   entry: './src/index.js',
   mode: 'production',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
   },
 };
