02c51c3558b8ee4eddf9aba592b1ce90c0c43b76
Valentin Laurin <valentin.laurin@quickcase.app>
2023-09-14 17:55:44 +0100

    Build both ESM and UMD distributables
    Needed to support both ESM and CJS in consumers of the library

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,64 @@ import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default {
-  entry: './src/index.js',
-  mode: 'production',
-  output: {
-    filename: 'main.js',
-    path: resolve(__dirname, 'dist'),
-    library: {
-      name: 'QuickCaseSDK',
-      type: 'commonjs-static',
+export default [
+  {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+      filename: 'main.js',
+      path: resolve(__dirname, 'dist'),
+      library: {
+        name: 'QuickCaseSDK',
+        type: 'umd',
+      },
+      globalObject: 'this',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env']
-            ]
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env']
+              ]
+            }
           }
         }
-      }
-    ],
+      ],
+    },
   },
-};
+  {
+    entry: './src/index.js',
+    mode: 'production',
+    experiments: {
+      outputModule: true,
+    },
+    output: {
+      filename: 'main.mjs',
+      path: resolve(__dirname, 'dist'),
+      library: {
+        type: 'module',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env']
+              ]
+            }
+          }
+        }
+      ],
+    },
+  },
+];
