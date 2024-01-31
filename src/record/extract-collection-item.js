3b9eaabf12b8eb49457f8f37d2f1056a3db578e0
Valentin Laurin <valentin.laurin@quickcase.app>
2024-01-31 11:31:18 +0000

    [Record] Add `extractCollectionItem()`
    Decorator for `extractValue()` to extract members relative to a parent complex field.

---
+++ src/record/extract-collection-item.js
@@ -0,0 +1,38 @@
+/**
+ * Creates an instance of a relative value extractor for items of a collection field.
+ * This can be further combined with `extractMember` to extract members of complex collection items.
+ *
+ * @param {function} extract - Primed extract function, either `extractValue` or `extractMember`
+ * @param {string} collectionPath - Path to parent collection field
+ * @returns {function(*): *} Extract function relative to collection parent
+ */
+const extractCollectionItem = (extract, collectionPath) => (path) => {
+  const resolver = resolvePath(collectionPath);
+  return extract(processPath(resolver)(path));
+};
+
+const resolvePath = (parentPath) => (path) => {
+  if (Number.isInteger(path)) {
+    return `${parentPath}[${path}].value`;
+  }
+
+  if (path[0] === '[') {
+    return path;
+  }
+
+  return `${parentPath}[id:${path}].value`;
+};
+
+const processPath = (resolve) => (path) => {
+  if (Array.isArray(path)) {
+    return path.map(resolve);
+  } else if (typeof path === 'object' && path !== null) {
+    return Object.fromEntries(
+      Object.entries(path).map(([k, p]) => [k, resolve(p)])
+    );
+  }
+
+  return resolve(path);
+};
+
+export default extractCollectionItem;
---
+++ src/record/extract-collection-item.test.js
@@ -0,0 +1,80 @@
+import extractValue from './extract-value.js';
+import extractCollectionItem from './extract-collection-item.js';
+
+const extractor = extractValue({
+  id: '1111222233334444',
+  data: {
+    'collection1': [
+      {id: 'item-1', value: 'Value 1'},
+      {id: 'item-2', value: 'Value 2'},
+      {id: 'item-3', value: 'Value 3'},
+    ],
+  },
+});
+
+describe('when single item path', () => {
+  test('should extract item by ID', () => {
+    expect(
+      extractCollectionItem(extractor, 'collection1')('item-1')
+    ).toEqual('Value 1');
+  });
+
+  test('should extract item by index (0-based)', () => {
+    expect(
+      extractCollectionItem(extractor, 'collection1')(2)
+    ).toEqual('Value 3');
+  });
+
+  test('should extract metadata from root', () => {
+    expect(
+      extractCollectionItem(extractor, 'collection1')('[id]')
+    ).toEqual('1111222233334444');
+  });
+});
+
+describe('when array of item paths', () => {
+  test('should extract items from collection in same order', () => {
+    expect(extractCollectionItem(extractor, 'collection1')([
+      'item-2', // By ID
+      0, // By index
+      'item-3',
+    ])).toEqual([
+      'Value 2',
+      'Value 1',
+      'Value 3',
+    ]);
+  });
+
+  test('should extract metadata from root', () => {
+    expect(
+      extractCollectionItem(extractor, 'collection1')(['item-1', '[id]'])
+    ).toEqual([
+      'Value 1',
+      '1111222233334444',
+    ]);
+  });
+});
+
+describe('when object of item paths', () => {
+  test('should extract items from collection with matching shape', () => {
+    expect(extractCollectionItem(extractor, 'collection1')({
+      '1': 1, // By index
+      '2': 'item-1', // By ID
+      '3': 2,
+    })).toEqual({
+      '1': 'Value 2',
+      '2': 'Value 1',
+      '3': 'Value 3',
+    });
+  });
+
+  test('should extract metadata from root', () => {
+    expect(extractCollectionItem(extractor, 'collection1')({
+      '1': 'item-2',
+      '2': '[id]',
+    })).toEqual({
+      '1': 'Value 2',
+      '2': '1111222233334444',
+    });
+  });
+});
