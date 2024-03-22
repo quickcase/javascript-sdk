f0a8de75a87978ed757ed27ec2274867713dd95a
Valentin Laurin <valentin.laurin@quickcase.app>
2024-03-22 17:28:57 +0000

    [Path] Expose Path module
    Now contains elements useful for public consumption

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * as Condition from './condition/index.js';
 export * as Metadata from './metadata/index.js';
+export * as Path from './path/index.js';
 export * as Record from './record/index.js';
 export * as Template from './template/index.js';
